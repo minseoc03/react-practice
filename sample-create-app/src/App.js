a5700a50aef9dd1198938eb215882f6088eea82c
Minseo Choi <minseo_c03@MacBook-Air-10.local>
2023-08-12 13:28:46 +0900

    practiced state usage

--- sample-create-app/src/App.js
+++ sample-create-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper'
+import Counter from './Counter'
 
 //App component는 부모, Hello component는 자식
 //그렇기 때문에 Hello component에서 일어나는 일이 App 까지 영향을 끼치지 않음
@@ -10,6 +11,7 @@ function App() {
     <Wrapper>
       <Hello name = "React!" />
       <Hello name = "React!" />
+      <Counter/>
     </Wrapper>
   );
 }
